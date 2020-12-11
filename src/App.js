41045dc9baac551da0409b6be76c565a8be1618b
Jukka Aho <ahojukka5@gmail.com>
2020-12-11 19:55:24 +0700

    Use semantic-ui-react in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Container, Header } from 'semantic-ui-react';
 
-const App = () => <div>hello webpack</div>;
+const App = () => (
+  <Container text>
+    <Header as="h1">hello webpack</Header>
+  </Container>
+);
 
 export default App;
