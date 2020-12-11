32716f90fbd7c991fe574c1899748f22d0cc4b67
Jukka Aho <ahojukka5@gmail.com>
2020-12-11 17:49:31 +0700

    Render App using ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const hello = (name) => {
-  console.log(`hello ${name}`);
-};
-
-hello('ahojukka5');
-
-App();
+ReactDOM.render(<App />, document.getElementById('root'));
