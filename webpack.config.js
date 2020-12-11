2db24135a8a0a3d06d95315652655400afa5bc4b
Jukka Aho <ahojukka5@gmail.com>
2020-12-11 16:24:51 +0700

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+  },
+};
+module.exports = config;
