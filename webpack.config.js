8975f4625b60d6110c9f710d6d57b65d145c0717
Jukka Aho <ahojukka5@gmail.com>
2020-12-11 17:24:45 +0700

    Use clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Webpack test page',
     }),
